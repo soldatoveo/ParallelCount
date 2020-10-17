631cf7a76c38276d6e9f2c58982028c0892eb6f4
Евгений Солдатов <soldatov.eo@gmail.com>
2020-10-17 11:32:33 +0700

    Add Project

---
+++ ParallelCount/CountServer.cs
@@ -0,0 +1,66 @@
+﻿namespace ParallelCount
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    ///     Сервер счетчик
+    /// </summary>
+    public static class CountServer
+    {
+        /// <summary>
+        ///     Локер
+        /// </summary>
+        private static readonly ReaderWriterLockSlim RwLockSlim = new ReaderWriterLockSlim();
+
+        /// <summary>
+        ///     Счетчик
+        /// </summary>
+        private static int count;
+
+        /// <summary>
+        ///     Прибавляет целочисленное значение к счетчику
+        /// </summary>
+        /// <param name="value"> целочисленное значение </param>
+        public static void AddToCount(int value)
+        {
+            RwLockSlim.EnterWriteLock();
+            try
+            {
+                Console.WriteLine($"[Writing] [Thread Id: {Thread.CurrentThread.Name}] Add: {value}");
+                count += value;
+
+                // Имитация работы
+                var rnd = new Random();
+                Thread.Sleep(rnd.Next(5, 30));
+            }
+            finally
+            {
+                RwLockSlim.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        ///     Возвращает значение счетчика
+        /// </summary>
+        /// <returns> Значение счетчика </returns>
+        public static int GetCount()
+        {
+            RwLockSlim.EnterReadLock();
+            try
+            {
+                Console.WriteLine($"[Reading] [Thread Id: {Thread.CurrentThread.Name}] Current Value: {count}");
+
+                // Имитация работы
+                var rnd = new Random();
+                Thread.Sleep(rnd.Next(5, 30));
+
+                return count;
+            }
+            finally
+            {
+                RwLockSlim.ExitReadLock();
+            }
+        }
+    }
+}
---
+++ ParallelCount/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ParallelCount
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var outputFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "output.txt");
+            var fileStream = new FileStream(outputFilePath, FileMode.Create);
+            var writerStream = new StreamWriter(fileStream);
+            Console.SetOut(writerStream);
+
+            var r1 = new Task(Read);
+            var r2 = new Task(Read);
+            var r3 = new Task(Read);
+            var w1 = new Task(Write);
+            var w2 = new Task(Write);
+            var tasks = new[] {r1, r2, r3, w1, w2};
+
+            r1.Start();
+            r2.Start();
+            r3.Start();
+            Thread.Sleep(20);
+            w1.Start();
+            w2.Start();
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void Read()
+        {
+            Parallel.For(0, 100, (i, state) =>
+            {
+                if (Thread.CurrentThread.Name == null)
+                {
+                    var name = new string(Guid.NewGuid().ToString().Where(char.IsLetterOrDigit).ToArray());
+                    Thread.CurrentThread.Name = name;
+                }
+
+                CountServer.GetCount();
+            });
+        }
+
+        private static void Write()
+        {
+            Parallel.For(0, 50, (i, state) =>
+            {
+                if (Thread.CurrentThread.Name == null)
+                {
+                    var name = new string(Guid.NewGuid().ToString().Where(char.IsLetterOrDigit).ToArray());
+                    Thread.CurrentThread.Name = name;
+                }
+
+                var digit = int.Parse(Guid.NewGuid().ToString().Where(char.IsDigit).First(ch => ch > '0').ToString());
+                CountServer.AddToCount(digit);
+            });
+        }
+    }
+}
